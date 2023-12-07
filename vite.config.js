22d714f7f73b9c7ec01988c90a9ebd555bb6ea5a
MR. HASAN <hasanshahriar32@gmail.com>
2023-12-07 10:08:15 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Specify the entry points for your application
   build: {
-    outDir: 'public/build', // Output directory for the built files
+    outDir: 'build', // Output directory for the built files
     manifest: true, // Generate manifest.json for better asset management
     rollupOptions: {
       input: {
